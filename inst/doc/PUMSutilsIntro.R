6893326e6e06e36792802c7b51f2ecbbba06e6df
David Thaler <davidthaler@gmail.com>
2017-02-13 13:32:09 -0800

    first commit with vignettes completed
    adding a few last minute mods to the vignettes onto the same commit

---
+++ inst/doc/PUMSutilsIntro.R
@@ -0,0 +1,34 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+library(dplyr)
+library(PUMSutils)
+
+## ----gold, echo=FALSE----------------------------------------------------
+# Ground truth for Washington State
+wa.gold[c(1, 7),]
+
+## ----noPUMSutils---------------------------------------------------------
+# Sum the weights to estimate a count
+sum(wa.house$WGTP)
+
+## ----estimate------------------------------------------------------------
+estimate(wa.house)
+
+## ----se------------------------------------------------------------------
+se.estimate(wa.house)
+
+## ----rentals-------------------------------------------------------------
+estimate(filter(wa.house, TEN==3))
+
+## ----se.rentals----------------------------------------------------------
+se.estimate(filter(wa.house, TEN==3))
+
+## ----prop----------------------------------------------------------------
+proportion(filter(wa.house, TEN==3), wa.house)
+
+## ----se.prop-------------------------------------------------------------
+se.proportion(filter(wa.house, TEN==3), wa.house)
+
+## ----table---------------------------------------------------------------
+acs.table(wa.house, 'TEN')
+
