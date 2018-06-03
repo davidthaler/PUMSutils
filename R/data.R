6b28453586219c36905a24917eeca70869e338e6
David Thaler <davidthaler@gmail.com>
2018-06-02 22:40:08 -0700

    Changed to 2016 ACS and updated tests/doc/vignettes

--- R/data.R
+++ R/data.R
@@ -1,31 +1,30 @@
-#' Ground Truth for 2015 ACS Washington Housing Data
-#' 
-#' 2015 ACS PUMS ground truth point estimates and standard errors for 12 housing characteristics for Washington State.
-#' 
-#' @format A data frame with 4 variables: 
+#' Ground Truth for 2016 ACS Washington Housing Data
+#'
+#' 2016 ACS PUMS ground truth point estimates and standard errors for 12 housing characteristics for Washington State.
+#'
+#' @format A data frame with 4 variables:
 #' \describe{
 #' \item{characteristic}{name of estimated characteristic in 2015 ACS}
-#' \item{pums_est_15}{point estimate of the charcteristic}
-#' \item{pums_se_15}{standard error of estimate}
-#' \item{pums_moe_15}{90\% confidence interval for estimate}
+#' \item{pums_est_16}{point estimate of the charcteristic}
+#' \item{pums_se_16}{standard error of estimate}
 #' }
-#' 
-#' @source 
+#'
+#' @source
 #' \url{http://www.census.gov/programs-surveys/acs/technical-documentation/pums/documentation.html} {PUMS Estimates for User Verification}
-'wa.gold'
+'wa.gold16'
 
-#' 2015 ACS Washington Housing Data
-#' 
-#' Parital 2015 ACS PUMS housing data for Washington State.
-#' Data has 32821 rows (all rows), but only 83 columns.
-#' Included variables are GRNTP, HINCP, NP, VACS, TEN, 
+#' 2016 ACS Washington Housing Data
+#'
+#' Parital 2016 ACS PUMS housing data for Washington State.
+#' Data has 33241 rows (all rows), but only 86 columns.
+#' Included variables are GRNTP, HINCP, NP, VACS, TEN,
 #' the expansion weight (WGTP), and the 80 replicate weights.
-#' VACS and TEN are the only variables used in the ACS PUMS estimates 
-#' for user verification, and the expansion weights and replicate weights 
+#' VACS and TEN are the only variables used in the ACS PUMS estimates
+#' for user verification, and the expansion weights and replicate weights
 #' are needed to compute point estimates and standard errors.
-#' The other variables are actual numbers (not categorical) 
+#' The other variables are actual numbers (not categorical)
 #' and are used in tests and vignettes.
-#' 
+#'
 #' @format A data frame with 86 variables:
 #' \describe{
 #' \item{NP}{number of persons in household}
@@ -36,6 +35,6 @@
 #' \item{WGTP}{expansion weight, the number of household in the population represented by this row}
 #' \item{wgtp1..wgtp80}{80 replicate weights for computing standard errors}
 #' }
-#' 
-#' @references 2015 ACS PUMS Data Dictionary \url{http://www.census.gov/programs-surveys/acs/technical-documentation/pums/documentation.html}
-'wa.house'
+#'
+#' @references 2016 ACS PUMS Data Dictionary \url{http://www.census.gov/programs-surveys/acs/technical-documentation/pums/documentation.html}
+'wa.house16'
--- tests/testthat/test-PUMSutils.R
+++ tests/testthat/test-PUMSutils.R
@@ -2,26 +2,26 @@ library(dplyr)
 context('PUMSutils')
 
 test_that('estimate() produces correct total', {
-  tot.HU <- wa.gold$pums_est_15[1]
-  rentals <- wa.gold$pums_est_15[7]
-  expect_equal(estimate(wa.house), tot.HU)
-  expect_equal(estimate(filter(wa.house, TEN==3)), rentals)
+  tot.HU <- wa.gold16$pums_est_16[1]
+  rentals <- wa.gold16$pums_est_16[7]
+  expect_equal(estimate(wa.house16), tot.HU)
+  expect_equal(estimate(filter(wa.house16, TEN==3)), rentals)
 })
 
 test_that('se.estimate() gives correct standard errors', {
-  se.HU <- wa.gold$pums_se_15[1]
-  se.rentals <- wa.gold$pums_se_15[7]
-  expect_equal(round(se.estimate(wa.house)), se.HU)
-  expect_equal(round(se.estimate(filter(wa.house, TEN==3))), se.rentals)
+  se.HU <- wa.gold16$pums_se_16[1]
+  se.rentals <- wa.gold16$pums_se_16[7]
+  expect_equal(round(se.estimate(wa.house16)), se.HU)
+  expect_equal(round(se.estimate(filter(wa.house16, TEN==3))), se.rentals)
 })
 
 test_that('proportion() gives correct fraction', {
-  tot.HU <- wa.gold$pums_est_15[1]
-  rentals <- wa.gold$pums_est_15[7]
-  gold.prop <- with(wa.gold, pums_est_15[7]/pums_est_15[1])
-  expect_equal(proportion(filter(wa.house, TEN==3), wa.house), gold.prop)
+  tot.HU <- wa.gold16$pums_est_16[1]
+  rentals <- wa.gold16$pums_est_16[7]
+  gold.prop <- with(wa.gold16, pums_est_16[7]/pums_est_16[1])
+  expect_equal(proportion(filter(wa.house16, TEN==3), wa.house16), gold.prop)
 })
 
 test_that('acs.table() gives correct value', {
-  expect_equivalent(acs.table(wa.house, 'TEN')[3], wa.gold$pums_est_15[7])
+  expect_equivalent(acs.table(wa.house16, 'TEN')[3], wa.gold16$pums_est_16[7])
 })
