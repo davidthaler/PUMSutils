d57abcd0c1703a42aef2802854dfb66fddb3aba8
David Thaler <davidthaler@gmail.com>
2018-06-22 18:04:05 -0700

    bugfix on acs.se; also doc update

--- R/acs.se.R
+++ R/acs.se.R
@@ -2,11 +2,11 @@
 #'
 #' This function uses weight replicates to compute direct standard errors
 #' on the ACS PUMS data for any statistic.
-#' This function is mainly for internal use (the function signature is tricky),
+#' It is mainly for internal use (the function signature is tricky),
 #' but it can be used to compute standard errors for any statistic,
 #' inclding user-defined ones, so it is exported.
 #' The argument `f` is a function that takes only one argument,
-#' which is a weight replicate number from 1...80.
+#' called wt.rep.num, which is a weight replicate number from 1...80.
 #' That means that it must already be bound to any data and paramters it needs.
 #' It should return the value of the statistic over its data,
 #' based on the weight replicate number used.
@@ -14,7 +14,8 @@
 #' See the ACS PUMS readme document referenced below for the details of
 #' the calculation of the direct standard errors.
 #'
-#' @param f a function that takes a weight replicate
+#' @param f a function that takes a weight replicate number called wt.rep.num,
+#'          with a default value of NULL
 #'
 #' @return the standard error of the statistic defined by f over the data
 #'
@@ -26,7 +27,7 @@
 #' se.acs.mean(wa.house16, 'GRNTP')
 #'
 #' # using a closure
-#' my.closure <- function(x, field) function(n=NULL) acs.mean(x, f, n)
+#' my.closure <- function(x, field) function(wt.rep.num=NULL) acs.mean(x, f, n)
 #' f <- my.closure(wa.house16, 'GRNTP')
 #' f()
 #' acs.se(f)
@@ -40,6 +41,6 @@
 #'
 acs.se <- function(f){
   pt.est <- f()
-  rep.ests <- sapply(1:80, f)
+  rep.ests <- sapply(1:80, function(n) f(wt.rep.num=n))
   sqrt((4/80) * sum((rep.ests - pt.est)^2))
 }
