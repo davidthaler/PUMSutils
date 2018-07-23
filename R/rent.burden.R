ffd9aa02f1709b5ee9f202d2e8f777fc85989adb
David Thaler <davidthaler@gmail.com>
2018-07-22 20:14:49 -0700

    Added rent.burden, another small function that I roll by hand a lot.

---
+++ R/rent.burden.R
@@ -0,0 +1,24 @@
+#' Define rent burden based on gross rent as a percent of household income.
+#'
+#' Households are said to be rent-burdened if their rent is more than 30%
+#' of household income.
+#' This function cuts the variable \code{GRPIP} in the housing data,
+#' which is gross rent as a percentage of household income, into bins
+#' for rent below 30%, 30% to 40%, 40% to 50% and above 50% of income.
+#'
+#' @param house a data frame of PUMS housing data
+#'
+#' @return factor column with levels of rent burden for households
+#'
+#' @import Hmisc cut2
+#'
+#' @examples
+#' wa.house16$Rent.Burden <- rent.burden(wa.house16)
+#' group.count(wa.house16, 'Rent.Burden', drop.na.level=TRUE)
+#'
+#' @export
+rent.burden <- function(house){
+  out <- cut2(house$GRPIP, cuts=c(30, 40, 50))
+  levels(out) <- c('No Burden', '30%-39%', '40%-49%', '50%+')
+  out
+}
