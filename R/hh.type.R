35555612158eaeb77c2c31abac2218e691380ef6
David Thaler <davidthaler@gmail.com>
2018-08-06 19:18:58 -0700

    Added hh.type.R...I use it all of the time, and not having it in PUMSutils is a hassle for me.

---
+++ R/hh.type.R
@@ -0,0 +1,25 @@
+#' Classify households by type
+#'
+#' Classifies each household as vacant, one person, a couple, a family with
+#' children present, or a composite household (none of the above).
+#' Note that \code{house} has to be full ACS PUMS housing data, not the
+#' subset of columns used in \code{wa.house16}.
+#'
+#' @param house ACS housing data or subset
+#'
+#' @return character vector of household type
+#'
+#' @importFrom dplyr case_when
+#'
+#' @export
+hh.type <- function(house){
+  with(house,
+        case_when(
+          NP==0 ~ 'Vacant',
+          NP==1 ~ 'One.Person',
+          (NP==2) & (HHT==1 | PARTNER %in% 1:4) ~ 'Couple',
+          HUPAC %in% 1:3 ~ 'Family',
+          TRUE ~ 'Composite'
+          )
+       )
+}
